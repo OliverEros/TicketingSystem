50ccfbd99f934bbb33420452fb7cabfbf9c9a20d
OliverEros <olivereros@protonmail.com>
2021-01-03 05:10:03 +0000

    TODO

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import axios from 'axios';
+import './MainPage.css'
 
 //Components
 import MainHeader from '../MainHeader/MainHeader';
@@ -7,44 +8,31 @@ import TicketContainer from '../DisplayTickets/TicketContainer'
 import FilterBar from '../FilterBar/FilterBar';
 
 
-class MainPage extends React.Component {
+
+
+class HomePage extends React.Component {
     constructor(props){
         super(props);
-        this.state = {
+        //All data is loaded here and then passed to each
+	//individiual compoment
+	    this.state = {
             user : [],
             tickets : [],
             groups : [],
-
-            whichTickets : ''
            
-
         }
     }
 
     componentDidMount() {
-        axios.get('http://localhost:3000/loadUserData', {withCredentials: true})
-        .then((userData) => {
-            this.setState({user : userData.data, 
-                        tickets : userData.data.tickets,
-                    groups : userData.data.joinedGroups})
-        })
-        .catch((err) => {
-            console.log(err)
-        })
     }
 
-    filter(f){
-        this.setState({whichTickets : f})
-    }
-    
-    
 
     render(){
         return(
             <div className = "MainPage">
                 <MainHeader />
-                <div className="row">
-                <FilterBar groups = {this.state.groups} />
+                <div className='container-fluid'>
+
                 <TicketContainer />
                 </div>
             </div>
@@ -53,5 +41,5 @@ class MainPage extends React.Component {
 
 }
 
-export default MainPage;
+export default HomePage;
 
