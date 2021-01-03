403a37cbdecfcd18c49c5313109a6bfc5c4d3e6d
OliverEros <olivereros@protonmail.com>
2021-01-03 04:57:57 +0000

    Added routes

--- server/app.js
+++ server/app.js
@@ -23,6 +23,8 @@ var ticketRouter = require('./routes/ticketRouter');
 var loadTicketsRouter = require('./routes/loadTickets');
 var loadUserData = require('./routes/getUserData');
 var createGroup = require('./routes/createGroup');
+var isAuthenticated = require('./routes/isAuthenticated')
+var getCharData = require('./routes/getChartData');
 
 //Used for react to authenticate user access
 var authReact = require('./routes/isAuthenticated');
@@ -31,7 +33,7 @@ const flash = require('express-flash');
 
 var app = express();
 
-
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -57,6 +59,8 @@ app.use('/loadTickets', loadTicketsRouter);
 app.use('/loadUserData', loadUserData);
 app.use('/createGroup',createGroup);
 app.use('/auth4React', authReact );
+app.use('/isAuth', isAuthenticated);
+app.use('/getChartData', getCharData);
 
 
 // catch 404 and forward to error handler
