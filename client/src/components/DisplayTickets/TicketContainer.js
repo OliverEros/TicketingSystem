47163633d9c0b9e4ac2e839618fee10df2bda9cb
OliverEros <olivereros@protonmail.com>
2021-01-03 05:14:06 +0000

    Created pagination

--- client/src/components/DisplayTickets/TicketContainer.js
+++ client/src/components/DisplayTickets/TicketContainer.js
@@ -1,9 +1,7 @@
-import React from 'react'
 import axios from 'axios'
+import React from 'react'
 import './TicketContainer.css'
-import TicketCards from '../IndTickets/IndTickets'
 
-import {useHistory} from 'react-router-dom'
 
 
 
@@ -22,177 +20,131 @@ class TicketContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // Keeps track of which index the database should start
-            // loading tickets from
-            at: 0,
-            end: 4,
-            tickets: [],
-            sortedCards: [],
-
-
-            alltickets : true,
-            pendingTickets : false,
-            resolvedTickets : false,
-            groupTickets : false,  
-
-            groupToSort : '',
+            //Keeps track of which filter is being used (all, pending, or resolved)
+            currentTicket: '',
+            numberofPages: 0,
+            atPage: 1,
+            tickets: []
         }
 
-        this.renderAllCards = this.renderAllCards.bind(this);
-        this.renderPendingCards = this.renderPendingCards.bind(this);
-        this.showButton = this.showButton.bind(this);
-        this.resultController = this.resultController.bind(this)
+        this.ALL = { resolved: undefined, department: 'sd', atPage: this.state.atPage }
+        this.PENDING = { resolved: false, department: 'sd', atPage: this.state.atPage }
+        this.RESOLVED = { resolved: true, department: 'sd', atPage: this.state.atPage }
+
+        this.fetchTickets = this.fetchTickets.bind(this);
+        this.returnTickets = this.returnTickets.bind(this);
+        this.changePage = this.changePage.bind(this);
     }
 
+    fetchTickets = (query) => {
+        axios.get('http://localhost:3000/loadTickets', { withCredentials: true, params: query })
+            .then(data => {
+                this.setState(() => {
+                    return {
+                        currentFilter: query,
+                        tickets: data.data.data,
+                        numberofPages: data.data.pages
+                    }
+                })
+                console.log(this.state.currentTicket)
+            })
+            .catch(err => { console.log(err) })
+    }
 
     componentDidMount() {
-        let loadedTickets = []
-        axios.get('http://localhost:3000/loadTickets')
-            .then((result) => {
-                result.data.forEach(element => {
-                    loadedTickets.push(element)
-                })
-                return loadedTickets;
-            })
-            .then((arrayToCopy) => {
-                this.setState({ tickets: [...this.state.tickets, ...arrayToCopy] })
-            })
-            .catch((err) => { console.log(err) })
+        this.fetchTickets(this.ALL)
+
     }
 
     /**
-     *  Data is passed from the filter component
-     *  Decides which tickets will be shown
+     * Returns tickets based on the following parameters:
+     * ->All
+     * ->Pending
+     * ->Resolved
      */
-    resultController() {
-
-        const whichTickets = this.props.whichCards
-        
-        switch(whichTickets){
-            case 'all':
-                this.setState({alltickets : true, pendingTickets : false, resolvedTickets : false , groupTickets : false})
-                break;
-            case 'pending':
-                this.setState({alltickets : false, pendingTickets : true, resolvedTickets : false , groupTickets : false})
-                break
-            case 'resolved':
-                this.setState({alltickets : false, pendingTickets : false, resolvedTickets : true , groupTickets : false})
-                break;
-            case 'group':
-                break;
+    returnTickets() {
+        if (this.state.tickets.length == 0) {
+            return (<h1>No tickets to load</h1>)
+        } else {
+            return (this.state.tickets.map(ticket => {
+                let status;
+                if (ticket.resolved) {
+                    status = 'Resolved'
+                } else {
+                    status = 'Pending'
+                }
+                return (<li id='liTickets' className='list-group-item'>{ticket.title} <span class="badge badge-primary badge-pill" style={{ float: "right" }}>{status}</span></li>)
+            }))
         }
     }
-
     /**
-     *  Renders the required tickets
+     * Creates pages for pagination
      */
-    whichTickets(){
-        const state = this.state
-
-        if(state.alltickets === true) {
-          return  this.renderAllCards()
-           
-        } 
-        else if(state.pendingTickets === true){
-          return  this.renderPendingCards()
-    
-        }
-        else if(state.resolvedTickets === true){
-         return  this.renderResolvedCards()
+    returnPages() {
+        let _pageArray = []
+        //create a temporary array
+        if (this.state.numberofPages == 0) {
+            return (<div></div>)
+        } else {
+            for (let i = 1; i <= this.state.numberofPages; i++) {
+                _pageArray.push(<li class="page-item" onClick={(element) => this.changePage(element.target.id)}><a class="page-link" id={i} href="#">{i}</a></li>)
+            }
+            return (_pageArray.map(item => { return item }))
         }
     }
 
-
-     /**
-     * Displays a given amount of tickets
-     * 
-     */
-    renderAllCards() {
-        
-        const tickets = this.state.tickets.slice(this.state.at, this.state.end)
-
-        return tickets.map(ticket => {
-            return (
-                <div className="col-xs-4">
-                    <TicketCards title={ticket.title} user={ticket.createdBy} body={ticket.description} status={ticket.resolved} />
-                </div>
-            )
-        })
+    //Changes page for pagination
+    changePage(element) {
+        //create a copy of the currently saved filter
+        let filter = { ...this.state.currentFilter }
+        //Each page number has a value that is saved in the form of id. This value is passed to the fetchTicket function
+        // Tickets to load: [page = x, number of tickets = 10] => skip(page-1).limit(10)
+        filter['atPage'] = element
+        this.fetchTickets(filter)
     }
-
-   
-    /**
-     * Only renders cards with pending status
-     */
-    renderPendingCards(){
-        const pendingCards = []
-
-        this.state.tickets.forEach(card => {
-            if (card.resolved == false){
-                pendingCards.push(card)
-            }        
-        });
-
-        let nbmrOfCards = pendingCards.slice(this.state.at, this.state.end)
-
-       return nbmrOfCards.map(ticket => {
-            return (
-                
-                <div className="col-xs-4">
-                    <TicketCards title={ticket.title} user={ticket.createdBy} body={ticket.description} status={ticket.resolved} />
-                </div>
-            )
-        })
-    }
-
-     /**
-      *  Only show button if there are tickets to load
-      */
-     showButton = () => {
-        if (this.state.end < this.state.tickets.length) {
-            return <button onClick={this.loadMore}>Show More</button>
-        }
-    }
-
-     /**
-     *  Logic to load more tickets
-     */
-    loadMore = () => {
-        this.setState({ end: this.state.end + 6 }, () => {
-            this.whichTickets()
-        
-        })
-    }
-
-
 
 
     render() {
+        return (
 
-        if(this.state.tickets.length > 0 ){
-            return (
-                <div >
-                    <button onClick=  {() => this.resultController()}>resultController</button>
-                    <div className='row ticketcontainer md-4'>
-                        {this.whichTickets()}
-                    
-                    </div>
-                    <div id="buttoncontainer">
-                            {this.showButton()}
+            <div className='TicketContainer'>
+                <div className='row'>
+                    <div className='tabs col-sm-12'>
+                        <button id='allTickets' className='btn btn-primary btn-sm' onClick={() => this.fetchTickets(this.ALL)}>All</button>
+                        <button id='pendingTickets' className='btn btn-primary btn-sm' onClick={() => this.fetchTickets(this.PENDING)}>Pending</button>
+                        <button id='resolvedTickets' className='btn btn-primary btn-sm' onClick={() => this.fetchTickets(this.RESOLVED)}>Resolved</button>
                     </div>
                 </div>
-    
-            );
-        } else {
-            return(
-                <div className = "col sm-9">
-                    <h3>There are no tickets to show</h3>
+
+                <div className='row justify-content-center'>
+                    <div className='col-md-12 '>
+                        <div className='ticketList'>
+                            <ul id='ulTickets' className='list-group'>
+                                {this.returnTickets()}
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className='row justify-content-center'>
+                    <div className='col-md-12'>
+                        <div id='paginationContainer'>
+                            <ul className='pagination justify-content-center'>
+                                {this.returnPages()}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
-            )
-            
-        }
-        
+
+        )
     }
+
+
+
 }
 
 export default TicketContainer;
